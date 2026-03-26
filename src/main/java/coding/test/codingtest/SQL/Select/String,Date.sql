5eb144898c91e4c75c27db248f1f901524209d7f
최다빈 <9636515@gmail.com>
2026-03-26 17:58:51 +0900

    1일1 SQL 문제 풀기

--- src/main/java/coding/test/codingtest/SQL/Select/String,Date.sql
+++ src/main/java/coding/test/codingtest/SQL/Select/String,Date.sql
@@ -5,3 +5,108 @@ LEVEL1
 SELECT COUNT(*) AS FISH_COUNT
 FROM FISH_INFO
 WHERE YEAR(TIME) = 2021
+
+2. CAR_RENTAL_COMPANY_CAR 테이블에서 '네비게이션' 옵션이 포함된 자동차 리스트를 출력하는 SQL문을 작성해주세요. 결과는 자동차 ID를 기준으로 내림차순 정렬해주세요.
+SELECT *
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE '%네비게이션%'
+ORDER BY CAR_ID DESC;
+
+3. CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 대여 시작일이 2022년 9월에 속하는 대여 기록에 대해서 대여 기간이 30일 이상이면 '장기 대여'
+그렇지 않으면 '단기 대여' 로 표시하는 컬럼(컬럼명: RENT_TYPE)을 추가하여 대여기록을 출력하는 SQL문을 작성해주세요. 결과는 대여 기록 ID를 기준으로 내림차순 정렬해주세요.
+/* DATEDIFF(END, START = 날짜의 계산 하지만 차이만 계산하기에 + 1 해줘야함. */
+SELECT HISTORY_ID, CAR_ID, DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
+, DATE_FORMAT(END_DATE, '%Y-%m-%d')AS END_DATE,
+    CASE
+        WHEN DATEDIFF(END_DATE, START_DATE) + 1>= 30 THEN '장기 대여'
+        ELSE '단기 대여'
+END AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE START_DATE >= '2022-09-01'
+  AND START_DATE < '2022-10-01'
+ORDER BY HISTORY_ID DESC;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+LEVEL2
+
+1. USED_GOODS_BOARD 테이블에서 2022년 10월 5일에 등록된 중고거래 게시물의 게시글 ID, 작성자 ID, 게시글 제목, 가격, 거래상태를 조회하는 SQL문을 작성해주세요.
+거래상태가 SALE 이면 판매중, RESERVED이면 예약중, DONE이면 거래완료 분류하여 출력해주시고, 결과는 게시글 ID를 기준으로 내림차순 정렬해주세요.
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+CASE
+    WHEN STATUS = 'SALE' THEN '판매중'
+    WHEN STATUS = 'RESERVED' THEN '예약중'
+    WHEN STATUS = 'DONE' THEN '거래완료'
+END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE >= '2022-10-05'
+  AND CREATED_DATE < '2022-10-06'
+ORDER BY BOARD_ID DESC
+
+2.CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 평균 대여 기간이 7일 이상인 자동차들의 자동차 ID와 평균 대여 기간(컬럼명: AVERAGE_DURATION) 리스트를 출력하는 SQL문을 작성해주세요.
+평균 대여 기간은 소수점 두번째 자리에서 반올림하고, 결과는 평균 대여 기간을 기준으로 내림차순 정렬해주시고, 평균 대여 기간이 같으면 자동차 ID를 기준으로 내림차순 정렬해주세요.
+(CAR_ID별 평균을 구하기 위해 HAVING 사용)
+SELECT CAR_ID,
+       ROUND(AVG(DATEDIFF(END_DATE, START_DATE) + 1), 1) AS AVERAGE_DURATION
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+HAVING AVERAGE_DURATION >= 7
+ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC
+
+3. 동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN
+    ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', "Mitty"
+    )
+ORDER BY ANIMAL_ID
+
+4. 보호소에 돌아가신 할머니가 기르던 개를 찾는 사람이 찾아왔습니다. 이 사람이 말하길 할머니가 기르던 개는 이름에 'el'이 들어간다고 합니다. 동물 보호소에 들어온 동물 이름 중,
+이름에 "EL"이 들어가는 개의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 이름 순으로 조회해주세요. 단, 이름의 대소문자는 구분하지 않습니다.
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
+
+5. 보호소의 동물이 중성화되었는지 아닌지 파악하려 합니다. 중성화된 동물은 SEX_UPON_INTAKE 컬럼에 'Neutered' 또는 'Spayed'라는 단어가 들어있습니다.
+동물의 아이디와 이름, 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요. 이때 중성화가 되어있다면 'O', 아니라면 'X'라고 표시해주세요.
+    SELECT ANIMAL_ID, NAME,
+    CASE
+    WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+    WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+    ELSE 'X'
+END AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+6.PRODUCT 테이블에서 상품 카테고리 코드(PRODUCT_CODE 앞 2자리) 별 상품 개수를 출력하는 SQL문을 작성해주세요. 결과는 상품 카테고리 코드를 기준으로 오름차순 정렬해주세요.
+SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY,
+       COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY LEFT(PRODUCT_CODE, 2)
+ORDER BY CATEGORY ASC;
+
+7. ANIMAL_INS 테이블에 등록된 모든 레코드에 대해, 각 동물의 아이디와 이름, 들어온 날짜1를 조회하는 SQL문을 작성해주세요. 이때 결과는 아이디 순으로 조회해야 합니다.
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d')
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+8. AIR_POLLUTION 테이블에서 수원 지역의 연도 별 평균 미세먼지 오염도와 평균 초미세먼지 오염도를 조회하는 SQL문을 작성해주세요.
+이때, 평균 미세먼지 오염도와 평균 초미세먼지 오염도의 컬럼명은 각각 PM10, PM2.5로 해 주시고, 값은 소수 셋째 자리에서 반올림해주세요.
+결과는 연도를 기준으로 오름차순 정렬해주세요.
+SELECT
+    YEAR(YM) AS YEAR,
+    ROUND(AVG(PM_VAL1), 2) AS PM10,
+    ROUND(AVG(PM_VAL2), 2) AS `PM2.5`
+FROM AIR_POLLUTION
+WHERE LOCATION2 = '수원'
+GROUP BY YEAR(YM)
+ORDER BY YEAR ASC;
+
+9.각 분기(QUARTER)별 분화된 대장균의 개체의 총 수(ECOLI_COUNT)를 출력하는 SQL 문을 작성해주세요.
+이때 각 분기에는 'Q' 를 붙이고 분기에 대해 오름차순으로 정렬해주세요. 대장균 개체가 분화되지 않은 분기는 없습니다.
+SELECT
+    CONCAT(QUARTER(DIFFERENTIATION_DATE), 'Q') AS QUARTER,
+    COUNT(*) AS ECOLI_COUNT
+FROM ECOLI_DATA
+GROUP BY CONCAT(QUARTER(DIFFERENTIATION_DATE), 'Q')
+ORDER BY QUARTER;
