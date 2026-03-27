16a317fb00bf97986af328fcaf092da84dbe86ee
최다빈 <9636515@gmail.com>
2026-03-27 16:48:13 +0900

    다른 버전 생각

--- src/main/java/coding/test/codingtest/StackorQueue/N6.java
+++ src/main/java/coding/test/codingtest/StackorQueue/N6.java
@@ -16,26 +16,39 @@ public class N6 {
      * @param prices : 주식의 가격 변동 (1초당)
      * @return : index별로 몇초간 가격이 떨어지지 않고 유지되었는지 판별
      */
-
     public static int[] solution(int[] prices) {
-        Stack<Integer> stack = new Stack<>();
-        int[] result = new int[prices.length];
-
-        for (int i = 0; i < prices.length; i++) {
-            // 가격이 떨어졌을 때
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int prevIndex = stack.pop();
-                result[prevIndex] = i - prevIndex;
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
             }
-            stack.push(i);
         }
-
-        // 끝까지 안 떨어진 경우
-        while (!stack.isEmpty()) {
-            int index = stack.pop();
-            result[index] = prices.length - 1 - index;
-        }
-
-        return result;
+        return answer;
     }
+
+//    public static int[] solution(int[] prices) {
+//        Stack<Integer> stack = new Stack<>();
+//        int[] result = new int[prices.length];
+//
+//        for (int i = 0; i < prices.length; i++) {
+//            // 가격이 떨어졌을 때
+//            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+//                int prevIndex = stack.pop();
+//                result[prevIndex] = i - prevIndex;
+//            }
+//            stack.push(i);
+//        }
+//
+//        // 끝까지 안 떨어진 경우
+//        while (!stack.isEmpty()) {
+//            int index = stack.pop();
+//            result[index] = prices.length - 1 - index;
+//        }
+//
+//        return result;
+//    }
 }
