447c3f0685d1c8b754280f89429987efc9f967d5
최다빈 <9636515@gmail.com>
2026-03-27 15:27:20 +0900

    GROUP BY SQL 문제 풀이

---
+++ src/main/java/coding/test/codingtest/SQL/Group/Group.sql
@@ -0,0 +1,121 @@
+LEVEL2
+
+1. 상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을 총주문량이 작은 순서대로 조회하는 SQL 문을 작성해주세요.
+이때 총주문량을 나타내는 컬럼명은 TOTAL_ORDER로 지정해주세요.
+SELECT INGREDIENT_TYPE, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+FROM ICECREAM_INFO i
+JOIN FIRST_HALF f
+ON i.FLAVOR = f.FLAVOR
+GROUP BY INGREDIENT_TYPE
+ORDER BY TOTAL_ORDER ASC
+
+2. APPOINTMENT 테이블에서 2022년 5월에 예약한 환자 수를 진료과코드 별로 조회하는 SQL문을 작성해주세요.
+이때, 컬럼명은 '진료과 코드', '5월예약건수'로 지정해주시고 결과는 진료과별 예약한 환자 수를 기준으로 오름차순 정렬하고, 예약한 환자 수가 같다면 진료과 코드를 기준으로 오름차순 정렬해주세요.
+SELECT
+    MCDP_CD AS '진료과 코드',
+    COUNT(*) AS '5월예약건수'
+FROM APPOINTMENT
+WHERE APNT_YMD >= '2022-05-01 00:00:00'
+  AND APNT_YMD < '2022-06-01 00:00:00'
+GROUP BY MCDP_CD
+ORDER BY COUNT(*) ASC, MCDP_CD ASC;
+
+3. CAR_RENTAL_COMPANY_CAR 테이블에서 '통풍시트', '열선시트', '가죽시트' 중 하나 이상의 옵션이 포함된 자동차가 자동차 종류 별로 몇 대인지
+출력하는 SQL문을 작성해주세요. 이때 자동차 수에 대한 컬럼명은 CARS로 지정하고, 결과는 자동차 종류를 기준으로 오름차순 정렬해주세요.
+// IN을 사용하게 되면 완전히 일치하는 값을 찾기 때문에, 여러개의 문자열이 들어가있으면 LIKE를 사용
+SELECT CAR_TYPE, COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE '%통풍시트%'
+   OR OPTIONS LIKE '%열선시트%'
+   OR OPTIONS LIKE '%가죽시트%'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC;
+
+4. 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 이때 고양이를 개보다 먼저 조회해주세요.
+SELECT ANIMAL_TYPE,COUNT(*) AS count
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'Cat' OR ANIMAL_TYPE = 'Dog'
+GROUP BY ANIMAL_TYPE
+ORDER BY
+    CASE
+        WHEN ANIMAL_TYPE = 'Cat' THEN 1
+        WHEN ANIMAL_TYPE = 'Dog' THEN 2
+END;
+
+5. 동물 보호소에 들어온 동물 이름 중 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 SQL문을 작성해주세요.
+이때 결과는 이름이 없는 동물은 집계에서 제외하며, 결과는 이름 순으로 조회해주세요.
+SELECT NAME, COUNT(NAME) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+GROUP BY NAME
+HAVING COUNT(NAME) >=2
+ORDER BY NAME
+
+6.보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다.
+// HOUR, MINUTES, SECONDS다 존재함
+09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 이때 결과는 시간대 순으로 정렬해야 합니다.
+SELECT
+    HOUR(DATETIME) AS HOUR,
+    COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) >= 9
+  AND HOUR(DATETIME) < 20
+GROUP BY HOUR(DATETIME)
+ORDER BY HOUR(DATETIME);
+
+7. PRODUCT 테이블에서 만원 단위의 가격대 별로 상품 개수를 출력하는 SQL 문을 작성해주세요. 이때 컬럼명은 각각 컬럼명은 PRICE_GROUP, PRODUCTS로 지정해주시고 가격대 정보는
+각 구간의 최소금액(10,000원 이상 ~ 20,000 미만인 구간인 경우 10,000)으로 표시해주세요. 결과는 가격대를 기준으로 오름차순 정렬해주세요.
+// FLOOR : 소수점 떼고 버려버림, CEIL : 소수점 떼고 반올림
+SELECT
+    FLOOR(PRICE / 10000) * 10000 AS PRICE_GROUP,
+    COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY FLOOR(PRICE / 10000)
+ORDER BY PRICE_GROUP ASC;
+
+8. HR_DEPARTMENT, HR_EMPLOYEES, HR_GRADE 테이블에서 2022년도 한해 평가 점수가 가장 높은 사원 정보를 조회하려 합니다.
+2022년도 평가 점수가 가장 높은 사원들의 점수, 사번, 성명, 직책, 이메일을 조회하는 SQL문을 작성해주세요.
+2022년도의 평가 점수는 상,하반기 점수의 합을 의미하고, 평가 점수를 나타내는 컬럼의 이름은 SCORE로 해주세요.
+SELECT
+    SUM(g.SCORE) AS SCORE,
+    e.EMP_NO,
+    e.EMP_NAME,
+    e.POSITION,
+    e.EMAIL
+FROM HR_EMPLOYEES e
+JOIN HR_GRADE g ON e.EMP_NO = g.EMP_NO
+GROUP BY e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+ORDER BY SCORE DESC
+LIMIT 1;
+
+9. SUBWAY_DISTANCE 테이블에서 노선별로 노선, 총 누계 거리, 평균 역 사이 거리를 노선별로 조회하는 SQL문을 작성해주세요.
+총 누계거리는 테이블 내 존재하는 역들의 역 사이 거리의 총 합을 뜻합니다. 총 누계 거리와 평균 역 사이 거리의 컬럼명은 각각 TOTAL_DISTANCE,
+AVERAGE_DISTANCE로 해주시고, 총 누계거리는 소수 둘째자리에서, 평균 역 사이 거리는 소수 셋째 자리에서 반올림 한 뒤 단위(km)를 함께 출력해주세요.
+결과는 총 누계 거리를 기준으로 내림차순 정렬해주세요.
+SELECT
+    ROUTE,
+    CONCAT(ROUND(SUM(D_BETWEEN_DIST), 1), 'km') AS TOTAL_DISTANCE,
+    CONCAT(ROUND(AVG(D_BETWEEN_DIST), 2), 'km') AS AVERAGE_DISTANCE
+FROM SUBWAY_DISTANCE
+GROUP BY ROUTE
+ORDER BY SUM(D_BETWEEN_DIST) DESC;
+
+10. FISH_NAME_INFO에서 물고기의 종류 별 물고기의 이름과 잡은 수를 출력하는 SQL문을 작성해주세요.
+물고기의 이름 컬럼명은 FISH_NAME, 잡은 수 컬럼명은 FISH_COUNT로 해주세요.
+결과는 잡은 수 기준으로 내림차순 정렬해주세요.
+SELECT COUNT(*) AS FISH_COUNT, n.FISH_NAME
+FROM FISH_INFO i
+JOIN FISH_NAME_INFO n ON i.FISH_TYPE = n.FISH_TYPE
+GROUP BY n.FISH_NAME
+ORDER BY COUNT(*) DESC
+
+11. 월별 잡은 물고기의 수와 월을 출력하는 SQL문을 작성해주세요.
+잡은 물고기 수 컬럼명은 FISH_COUNT, 월 컬럼명은 MONTH로 해주세요.
+결과는 월을 기준으로 오름차순 정렬해주세요.
+단, 월은 숫자형태 (1~12) 로 출력하며 9 이하의 숫자는 두 자리로 출력하지 않습니다. 잡은 물고기가 없는 월은 출력하지 않습니다.
+SELECT COUNT(*) AS FISH_COUNT, MONTH(TIME) AS MONTH
+FROM FISH_INFO
+GROUP BY MONTH(TIME)
+ORDER BY MONTH(TIME) ASC
+
+12.
