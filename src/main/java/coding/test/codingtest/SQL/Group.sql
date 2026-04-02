07a6caf6a89b61913d79381bb4e65cf7f9122c04
최다빈 <9636515@gmail.com>
2026-04-02 20:46:02 +0900

    group by sql문

--- src/main/java/coding/test/codingtest/SQL/Group.sql
+++ src/main/java/coding/test/codingtest/SQL/Group.sql
@@ -149,3 +149,12 @@ WHERE FAVORITES = (
     WHERE FOOD_TYPE = r.FOOD_TYPE
 )
 ORDER BY FOOD_TYPE DESC;
+
+4. HR_DEPARTMENT와 HR_EMPLOYEES 테이블을 이용해 부서별 평균 연봉을 조회하려 합니다. 부서별로 부서 ID, 영문 부서명, 평균 연봉을 조회하는 SQL문을 작성해주세요.
+평균연봉은 소수점 첫째 자리에서 반올림하고 컬럼명은 AVG_SAL로 해주세요.
+결과는 부서별 평균 연봉을 기준으로 내림차순 정렬해주세요.
+SELECT h.DEPT_ID, h.DEPT_NAME_EN, ROUND(AVG(e.SAL), 0) AS avg_sal
+FROM HR_DEPARTMENT h
+JOIN HR_EMPLOYEES e ON h.DEPT_ID = e.DEPT_ID
+GROUP BY DEPT_ID
+ORDER BY AVG_SAL DESC
