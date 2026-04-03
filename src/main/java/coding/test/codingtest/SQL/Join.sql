4dfe4bed5d3f36b3385e1f5a0c67863feef11547
최다빈 <9636515@gmail.com>
2026-04-03 17:04:58 +0900

    Join SQL LEvel4

--- src/main/java/coding/test/codingtest/SQL/Join.sql
+++ src/main/java/coding/test/codingtest/SQL/Join.sql
@@ -77,3 +77,20 @@ AND c.CAR_ID NOT IN (
 AND FLOOR(c.DAILY_FEE * 30 * (1 - d.DISCOUNT_RATE / 100)) >= 500000
 AND FLOOR(c.DAILY_FEE * 30 * (1 - d.DISCOUNT_RATE / 100)) < 2000000
 ORDER BY FEE DESC, c.CAR_TYPE ASC, c.CAR_ID DESC;
+
+3.보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다. 보호소에 들어올 당시에는 중성화1되지 않았지만,
+보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+
+
+SELECT
+    o.ANIMAL_ID,
+    o.ANIMAL_TYPE,
+    o.NAME
+FROM ANIMAL_OUTS o
+         JOIN ANIMAL_INS i ON o.ANIMAL_ID = i.ANIMAL_ID
+WHERE i.SEX_UPON_INTAKE LIKE 'Intact%' -- 들어올 때는 중성화 X
+  AND (
+    o.SEX_UPON_OUTCOME LIKE 'Spayed%' OR
+    o.SEX_UPON_OUTCOME LIKE 'Neutered%'
+    )
+ORDER BY o.ANIMAL_ID;
