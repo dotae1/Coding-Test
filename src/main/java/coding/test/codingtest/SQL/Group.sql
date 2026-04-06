f539c7777fc0d84965a46bbb65af1f52965ec293
최다빈 <9636515@gmail.com>
2026-04-06 22:33:54 +0900

    Group SQL LEVEL4

--- src/main/java/coding/test/codingtest/SQL/Group.sql
+++ src/main/java/coding/test/codingtest/SQL/Group.sql
@@ -248,3 +248,23 @@ WHERE YEAR(s.SALES_DATE) = 2022
   AND MONTH(s.SALES_DATE) = 1
 GROUP BY a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
 ORDER BY a.AUTHOR_ID ASC, b.CATEGORY DESC;
+
+11. USER_INFO 테이블과 ONLINE_SALE 테이블에서 년, 월, 성별 별로 상품을 구매한 회원수를 집계하는 SQL문을 작성해주세요.
+결과는 년, 월, 성별을 기준으로 오름차순 정렬해주세요. 이때, 성별 정보가 없는 경우 결과에서 제외해주세요.
+SELECT
+    YEAR(s.SALES_DATE) AS YEAR,
+    MONTH(s.SALES_DATE) AS MONTH,
+    u.GENDER,
+    COUNT(DISTINCT u.USER_ID) AS USERS
+FROM USER_INFO u
+JOIN ONLINE_SALE s
+    ON u.USER_ID = s.USER_ID
+WHERE u.GENDER IS NOT NULL
+GROUP BY
+    YEAR(s.SALES_DATE),
+    MONTH(s.SALES_DATE),
+    u.GENDER
+ORDER BY
+    YEAR,
+    MONTH,
+    u.GENDER;
