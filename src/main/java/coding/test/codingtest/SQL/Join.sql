a4d6cdd36e9b9a931c411953f2521f6e5e24434f
최다빈 <9636515@gmail.com>
2026-04-13 17:12:51 +0900

    JOIN LEVEL4 SQL

--- src/main/java/coding/test/codingtest/SQL/Join.sql
+++ src/main/java/coding/test/codingtest/SQL/Join.sql
@@ -122,3 +122,16 @@ WHERE r.MEMBER_ID = (
     LIMIT 1
 )
 ORDER BY r.REVIEW_DATE ASC, r.REVIEW_TEXT ASC;
+
+6. DEVELOPERS 테이블에서 Front End 스킬을 가진 개발자의 정보를 조회하려 합니다. 조건에 맞는 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
+결과는 ID를 기준으로 오름차순 정렬해 주세요.
+// !=를 통해 스킬을 포함하는지 체크
+SELECT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D
+WHERE EXISTS (
+    SELECT 1
+    FROM SKILLCODES S
+    WHERE S.CATEGORY = 'Front End'
+      AND (D.SKILL_CODE & S.CODE) != 0
+)
+ORDER BY D.ID ASC;
